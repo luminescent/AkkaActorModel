95016ce0d820fcc86e9d0b69dcfb90be08260ab8
Christina Nicolau <christina.androne@gmail.com>
2015-05-06 22:37:53 +0100

    make script use Release build

--- ChatHelper/Script.fsx
+++ ChatHelper/Script.fsx
@@ -2,8 +2,8 @@
 let start (filePath:string) =
     System.Diagnostics.Process.Start(filePath) |> ignore
 
-let chatServer = __SOURCE_DIRECTORY__ + "/../ChatServer/bin/Debug/ChatServer.exe"
-let chatClient = __SOURCE_DIRECTORY__ + "/../ChatClient/bin/Debug/ChatClient.exe"
+let chatServer = __SOURCE_DIRECTORY__ + "/../ChatServer/bin/Release/ChatServer.exe"
+let chatClient = __SOURCE_DIRECTORY__ + "/../ChatClient/bin/Release/ChatClient.exe"
 
 chatServer |> start
 [0..2] |> Seq.iter (fun _ -> chatClient |> start)
